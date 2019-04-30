f5fe5acf9ebb40e38e5b052b8da274835f2afccc
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-04-30 22:30:48 +0100

    completed calculator

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ if calcultor_type == "b"
   puts "Please enter second number:"
   num2 = gets.chomp("")
   if operator == "/"
-    num1float = num1.to_f
-    num1string = num1float.to_s
-    sum = eval"#{num1string} #{operator} #{num2}"
+    num1 = num1.to_f
+    num1 = num1.to_s
+    sum = eval"#{num1} #{operator} #{num2}"
   else
     sum = eval"#{num1} #{operator} #{num2}"
   end
@@ -44,44 +44,44 @@ if calcultor_type == "bmi"
   puts "(M)etric or (I)mperial:"
   unit = gets.chomp("")
   if unit == "m"
-    puts "Please enter your weight in kg:"
+    puts "Please enter your weight in kilograms:"
     weight = gets.chomp("").to_f
-    puts "Please enter your height in m:"
+    puts "Please enter your height in metres:"
     height = gets.chomp("").to_f
-    height_square = height * height
-    sum = (weight / height_square)
+    height = height * height
+    sum = (weight / height).round(4)
   end
   if unit == "i"
-    puts "Please enter your weight in lbs:"
+    puts "Please enter your weight in pounds:"
     weight = gets.chomp("").to_f
     puts "Please enter your height in inches:"
     height = gets.chomp("").to_i
-    height_square = height * height
-    sum = ((weight / height_square) * 703)
+    height = height * height
+    sum = ((weight / height) * 703).round(4)
   end
   puts sum
 end
 
 # Trip Calculator
 
-if calcultor_type == "bmi"
-  puts "(M)etric or (I)mperial:"
-  unit = gets.chomp("")
-  if unit == "m"
-    puts "Please enter your weight in kg:"
-    weight = gets.chomp("").to_f
-    puts "Please enter your height in m:"
-    height = gets.chomp("").to_f
-    height_square = height * height
-    sum = (weight / height_square)
+if calcultor_type == "t"
+  puts "Please enter your journey distance in Miles:"
+  distance = gets.chomp("").to_f
+  puts "Please enter your vehicle's fuel efficiency in MPG:"
+  fuel_efficiency = gets.chomp("").to_f
+  puts "Please end your vehicle's cost per gallon in pounds:"
+  cost_per_gallon = gets.chomp("").to_i
+  puts "Please end your driving speed in MPH:"
+  speed = gets.chomp("").to_i
+  trip_time = (distance / speed).round(1)
+  cost = ""
+  if speed <= 60
+    cost = (distance / fuel_efficiency) * cost_per_gallon
+  else
+    fuel_efficiency = fuel_efficiency - 2*(speed-60)
+    if fuel_efficiency >= 0
+      cost = (distance / fuel_efficiency) * cost_per_gallon
+    end
   end
-  if unit == "i"
-    puts "Please enter your weight in lbs:"
-    weight = gets.chomp("").to_f
-    puts "Please enter your height in inches:"
-    height = gets.chomp("").to_i
-    height_square = height * height
-    sum = ((weight / height_square) * 703)
-  end
-  puts sum
+  puts "Your trip with take #{trip_time} hours and cost £#{cost.round(2)}"
 end
